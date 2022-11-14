535303e23b5facb3158679a7c91cec08ccb7ab0e
FedericoArielLotitto <federicoariellotitto@gmail.com>
2022-11-14 20:12:19 -0300

    Agrega endopoint de categorías

--- src/main/kotlin/com/example/tpBD/model/Categoria.kt
+++ src/main/kotlin/com/example/tpBD/model/Categoria.kt
@@ -1,6 +1,15 @@
 package com.example.tpBD.model
-//
-//class Categoria {
-//    var id = 0
-//    var
-//}
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "categoria")
+class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CATEGORIA")
+    var id = 0
+    @Column(name = "TIPO")
+    var nombre :String = ""
+}
