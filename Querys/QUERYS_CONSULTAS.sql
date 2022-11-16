3d2210359745264f0e84a26dd4baefb4fce05ba3
Martin Perez Magallanes <mperezmagallanes@estudiantes.unsam.edu.ar>
2022-11-16 12:40:10 -0300

    Se modifica queryus

--- Querys/QUERYS_CONSULTAS.sql
+++ Querys/QUERYS_CONSULTAS.sql
@@ -1,3 +1,70 @@
+/*
+===============================================================================================================================================================================================
+Listar datos para los menus de opciones
+===============================================================================================================================================================================================
+*/
+SELECT * FROM categoria
+select * from contenido  where ID_CONTENIDO = :id
+
+/*
+===============================================================================================================================================================================================
+SENTENCIAS PARA MANIPULAR INFORMACION
+===============================================================================================================================================================================================
+*/
+DELETE FROM CONTENIDO WHERE ID_CONTENIDO=:id
+UPDATE contenido SET TITULO = :titulo, EXTENSION = :extension WHERE ID_CONTENIDO = :id
+INSERT  CONTENIDO (TITULO,FECHA_PUBLICADO,EXTENSION,URL,ID_TIPO_CONTENIDO) VALUES(:titulo,:fechaPublicado, :extension, :url, :idTipo)
+
+/*
+===============================================================================================================================================================================================
+CONSULTAS DE REPORTERIA
+===============================================================================================================================================================================================
+*/
+
+-- Busca contenidos por categoria
+SELECT  distinct cont.*
+FROM se_clasifica_en claf
+INNER JOIN contenido cont ON cont.ID_CONTENIDO = claf.ID_CONTENIDO
+INNER JOIN categoria cat ON cat.ID_CATEGORIA = claf.ID_CATEGORIA
+WHERE cat.TIPO IN (:categoria)
+ORDER BY cont.ID_CONTENIDO desc;
+
+
+-- Busca contenidos por titulo.
+SELECT *
+FROM CONTENIDO
+WHERE TITULO LIKE '%:titulo%';
+
+
+-- Ordenar archivos (no importa su tipo o categoría) por cantidad de descargas de mayor a menor
+SELECT  CTN.ID_CONTENIDO
+       ,CTN.TITULO
+       ,CTN.EXTENSION
+       ,COUNT(ES_REPRODUCIDO.ID_CONTENIDO) CANTIDAD_VISUALIZACIONES
+       ,COUNT(ES_DESCARGADO.ID_CONTENIDO) CANTIDAD_DESCARGADOS
+       ,CLF.CATEGORIA
+FROM contenido CTN
+LEFT JOIN ES_DESCARGADO ON ES_DESCARGADO.ID_CONTENIDO = CTN.ID_CONTENIDO
+LEFT JOIN ES_REPRODUCIDO ON ES_REPRODUCIDO.ID_CONTENIDO = CTN.ID_CONTENIDO
+INNER JOIN
+   (
+      SELECT  ID_CONTENIDO
+            ,group_concat(cat.tipo) CATEGORIA
+      FROM SE_CLASIFICA_EN SCLF
+      INNER JOIN CATEGORIA CAT ON CAT.ID_CATEGORIA = SCLF.ID_CATEGORIA
+      GROUP BY  ID_CONTENIDO
+   ) CLF ON clf.id_contenido = ctn.id_contenido
+GROUP BY  CTN.ID_CONTENIDO
+ORDER BY COUNT(ES_REPRODUCIDO.ID_CONTENIDO)+COUNT(ES_DESCARGADO.ID_CONTENIDO) DESC;
+
+
+
+/*
+===============================================================================================================================================================================================
+VARIOS
+===============================================================================================================================================================================================
+*/
+
 -- Se lista los contenidos que son descargable
 select cont.*
 from contenido cont
@@ -10,18 +77,7 @@ from contenido
 where EXTENSION in('MP4','PDF');
 
 
--- Busca contenidos por categoria
-select distinct cont.*
-from se_clasifica_en claf
-         inner join contenido cont on cont.ID_CONTENIDO = claf.ID_CONTENIDO
-         inner join categoria cat on cat.ID_CATEGORIA = claf.ID_CATEGORIA
-where cat.TIPO in ('BASICO','ULTRA')
-order by cont.ID_CONTENIDO desc;
 
--- Busca contenidos por titulo.
-SELECT *
-FROM CONTENIDO
-WHERE TITULO LIKE '%N%';
 
 -- Inserta un documento
 INSERT  CONTENIDO (TITULO,FECHA_PUBLICADO,EXTENSION,URL,ID_TIPO_CONTENIDO) VALUES('AGREGACION DE CONTENIDO','2021-05-01','MP4','https://drive.google.com/drive/u/0/folders/AGREGACION DE CONTENIDO.MP4',1);
@@ -53,20 +109,6 @@ ORDER BY COUNT(ES_REPRODUCIDO.ID_CONTENIDO) DESC ;
 
 -- Cantidad de veces que se descargo y visualizaron los archivos
 -- Ordenar archivos (no importa su tipo o categoría) por cantidad de descargas de mayor a menor
-SELECT CTN.ID_CONTENIDO,CTN.TITULO,CTN.EXTENSION,COUNT(ES_REPRODUCIDO.ID_CONTENIDO) CANTIDAD_VISUALIZACIONES, COUNT(ES_DESCARGADO.ID_CONTENIDO) CANTIDAD_DESCARGADOS
-     ,CLF.CATEGORIA
-FROM contenido CTN
-         LEFT JOIN ES_DESCARGADO ON ES_DESCARGADO.ID_CONTENIDO = CTN.ID_CONTENIDO
-         LEFT JOIN ES_REPRODUCIDO ON ES_REPRODUCIDO.ID_CONTENIDO = CTN.ID_CONTENIDO
-         INNER JOIN(
-			select ID_CONTENIDO,group_concat(cat.tipo) CATEGORIA
-			from SE_CLASIFICA_EN SCLF
-			INNER JOIN CATEGORIA CAT ON CAT.ID_CATEGORIA = SCLF.ID_CATEGORIA
-			group by ID_CONTENIDO
-         ) CLF on clf.id_contenido = ctn.id_contenido
-GROUP BY CTN.ID_CONTENIDO
-ORDER BY COUNT(ES_REPRODUCIDO.ID_CONTENIDO) + COUNT(ES_DESCARGADO.ID_CONTENIDO) DESC;
-
 
 
 -- CANTIDAD DE CONTENIDO POR CLASIFICACION
